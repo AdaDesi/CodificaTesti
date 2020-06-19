e46dd5edd55579307bd6527d04b13e2ec2413071
AdaDesi <58034038+AdaDesi@users.noreply.github.com>
2020-06-19 11:44:43 +0200

    Add files via upload

---
+++ Progetto Desideri/progetto-esame.js
@@ -0,0 +1,12 @@
+
+function ZoomIn(id){
+    var img_id="img_"+id;
+    document.getElementById(img_id).width=600; 
+    document.getElementById(img_id).height=975; 
+}
+
+function ZoomOut(id){
+    var img_id="img_"+id;
+    document.getElementById(img_id).width=400; 
+    document.getElementById(img_id).height=600; 
+}
